114f2c5e12c2b831ba9452ba7146a3cca059bcce
danielatoda <daniela.toda@ifro.edu.br>
2022-11-02 11:18:11 -0400

    Tela principal

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+cor1 = "#F6A3C2"
+principal = Tk()
+principal.title = "Cadastro"
+principal.configure(background=cor1)
+principal.geometry("300x300+200+200")
+
+principal.mainloop()
